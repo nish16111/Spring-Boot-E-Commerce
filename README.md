f555a57ea4296bd16e1e6deb0a4b840377b22f17
Nishant Sirwani <nishantsirwani@gmail.com>
2024-07-29 16:55:48 +0530

    E-Commerce Website.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring-Boot-E-Commerce
+Java | Spring-Boot | Maven | ORM | Hibernate | Postman
+
+Developed a robust e-commerce website using the Spring-Boot Framework and advanced Java,
+incorporating features such as product management, shopping cart functionality to deliver a seamless
+online shopping experience.
---
+++ src/main/java/com/sirwani/ecom_project/EcomProjectApplication.java
@@ -0,0 +1,12 @@
+package com.sirwani.ecom_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomProjectApplication.class, args);
+	}
+}
